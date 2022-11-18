5f78786f2eb1a8838516b869a9755cadcea7748c
LZH <lzh@lzh.cm>
2022-11-18 13:25:18 +0800

    引入vant库

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-
+import router from './router'
+// 1. 引入你需要的组件
+import Vant from 'vant'
+// 2. 引入组件样式
+import 'vant/lib/index.css'
 // 加载全局样式
 import './styles/index.less'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: (h) => h(App)
 }).$mount('#app')
