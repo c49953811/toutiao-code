e3f77fc14018be09de49aaee9642455a9e3b6b7b
LZH <lzh@lzh.cm>
2022-11-19 07:05:20 +0800

    login

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,14 @@ const request = axios.create({
   baseUlr: 'http://toutiao.itheima.net/'
 })
 // 请求拦截器
-
+request.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 // 响应拦截器
 
 export default request
